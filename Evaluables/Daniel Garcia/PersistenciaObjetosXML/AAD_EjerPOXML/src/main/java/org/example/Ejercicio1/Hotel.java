b2f4076f9623e4a77b95b1b4d96f37319525c58f
Daniel Garcia Pascual <danielgarciapascual23@gmail.com>
2023-10-12 10:44:04 +0200

    Cambios package ejerciciosXML

---
+++ Evaluables/Daniel Garcia/PersistenciaObjetosXML/AAD_EjerPOXML/src/main/java/org/example/Ejercicio1/Hotel.java
@@ -0,0 +1,56 @@
+package org.example.Ejercicio1;
+
+import java.io.Serializable;
+
+public class Hotel implements Serializable {
+
+    private String nombre;
+    private String direccion;
+    private double precio;
+
+    //Constructor vacío
+    public Hotel() {
+    }
+
+    //Constructor con parámetros
+    public Hotel(String nombre, String direccion, double precio) {
+        this.nombre = nombre;
+        this.direccion = direccion;
+        this.precio = precio;
+    }
+
+    //Getters y setters
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public double getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
+
+    //Método para imprimir por pantalla
+    @Override
+    public String toString() {
+        return "Hotel{" +
+                "nombre='" + nombre + '\'' +
+                ", direccion='" + direccion + '\'' +
+                ", precio=" + precio +
+                '}';
+    }
+}
